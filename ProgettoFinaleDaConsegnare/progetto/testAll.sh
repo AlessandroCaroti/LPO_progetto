80e112d2b371150103dc84bcc896d524187e9b78
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-07-03 16:59:20 +0200

    aggiunto test sui metodi che falliscono

--- ProgettoFinaleDaConsegnare/progetto/testAll.sh
+++ ProgettoFinaleDaConsegnare/progetto/testAll.sh
@@ -2,6 +2,7 @@
 
 SRC_ROOT='./interpreter'
 TEST_INPUT='./testInput'
+TEST_FALITURE='./failure'
 
 OUT_TEST="./testOutput"
 
@@ -14,6 +15,13 @@ printf "%s %s/Main.java\n" "${JAVA_COMPILER}" "${SRC_ROOT}"
 ${JAVA_COMPILER} ${SRC_ROOT}/Main.java
 
 for filename in ${TEST_INPUT}/*.txt; do
+    name=${filename##*/}
+    base=${name%.txt}
+    printf "java interpreter.Main -i %s -o %s\n" "$filename" "${OUT_TEST}/${base}_OUT.txt"
+    java interpreter.Main -i $filename -o "${OUT_TEST}/${base}_OUT.txt"
+done
+
+for filename in ${TEST_FALITURE}/*.txt; do
     name=${filename##*/}
     base=${name%.txt}
     printf "java interpreter.Main -i %s -o %s\n" "$filename" "${OUT_TEST}/${base}_OUT.txt"
